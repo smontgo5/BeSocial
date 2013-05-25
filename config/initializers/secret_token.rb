ce1c9916fcde2f64c8eafbd114f1d7e3fb78b744
Spencer Montgomery <smontgo5@gmail.com>
2013-05-25 13:34:33 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BeSocial::Application.config.secret_token = 'b74a38223795e588d90727a420595398b1a14a6417f3e009154db8b5afe351615869527779ce6733ab15a9f7a20d6d7e9a2fcf0a7e59cf44d5bbb989e74cfa19'
