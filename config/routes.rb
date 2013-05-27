e7d661e2cbcb4b963a4f7639598e9c38588cb60f
Spencer Montgomery <smontgo5@gmail.com>
2013-05-27 15:22:43 -0700

    Change dev database to postgres

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ BeSocial::Application.routes.draw do
 
   get "static_pages/about"
 
+  get "static_pages/contact"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
